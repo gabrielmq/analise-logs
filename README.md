b8c6bd7a7fbd26bcc92dec21ff85c4b618bdc4b9
Gabriel <gabrielmqs04@gmail.com>
2019-03-09 14:06:27 -0300

    atualizando README e app.py

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Este projeto é uma ferramenta de relatórios que realiza consultas no banco de 
 
 - Clone o projeto `https://github.com/gabrielmq/analise-logs.git`
 - Acesse o diretório do projeto através do prompt de comando e execute o comando `docker-compose up` para que seja inicializado o container do banco de dados postgreSQL
-- Com o container inicializado, execute o comando `python3 app.py` pelo prompt de comandos para inicializar a ferramenta de logs
+- Com o container inicializado, dentro do diretorio do projeto execute o comando `python3 app.py` pelo prompt de comandos para inicializar a ferramenta de logs
 
 _Obs: para finalizar a execução do container execute o comando `docker-compose down` pelo prompt de comando_
 
--- app.py
+++ app.py
@@ -6,18 +6,17 @@
     printando algumas informações no console.
 """
 
-from appdb import get_artigos_populares
-from appdb import get_autores_populares
-from appdb import get_erros_requisicao
+import appdb 
+
 
 print("\n1. Quais são os três artigos mais populares de todos os tempos?")
-for artigo in get_artigos_populares():
+for artigo in appdb.get_artigos_populares():
     print("- {0} - {1} views".format(artigo[0], artigo[1]))
 
 print("\n2. Quem são os autores de artigos mais populares de todos os tempos?")
-for autor in get_autores_populares():
+for autor in appdb.get_autores_populares():
     print("- {0} - {1} views".format(autor[0], autor[1]))
 
 print("\n3. Em quais dias mais de 1% das requisições resultaram em erros?")
-for erros in get_erros_requisicao():
+for erros in appdb.get_erros_requisicao():
     print("- {0} - {1} erros".format(erros[0], erros[1]))
